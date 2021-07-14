6516f4f421688b59ad5128e2ee668d0c7bafcdee
BLACKPandatata <87409793+BLACKPandatata@users.noreply.github.com>
2021-07-14 00:51:34 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+pkg install git
+pkg install bash
+pkg install nodejs
+
+git clone https://github.com/BLACKPandatata/BlackBot/
+cd https://github.com/BLACKPandatata/BlackBot/
+bash install.sh / si cambiaron el nombre, escriban como le pusieron a su archivo
+npm start
