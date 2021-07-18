e8b96a94a5fab7227a94a8a6afcbc031e17fca06
BLACKPandatata <87409793+BLACKPandatata@users.noreply.github.com>
2021-07-18 02:25:20 -0500

    Update BlackPanda.js

--- BlackPanda.js
+++ BlackPanda.js
@@ -1,26 +1,94 @@
-# const { WAConnection } = require('@adiwajshing/webPanda');
+const { WAConnection, MessageType } = require('@adiwajshing/webPanda');
 const fs = require('fs');
+const prefix = '!'
 
 async function iniciar () { 
-        const client = new WAConnection()
+        const BlackPanda = new WAConnection()
+//Aquí el "BlackPanda" lo pueden cambiar a su gusto. Pero si cambian, tendrán que cambiar todos los "client" por el cambio que hicieron.
+        BlackPanda.logger.level = 'warn'
 
-        client.logger.level = 'warn'
-
-        client.on('qr', () => {
+//llamar al código QR
+        BlackPanda.on('qr', () => {
         })
 
-        fs.existsSync('./BlackPanda.json') && client.loadAuthInfo('./BlackPanda.json')
+//crear un archivo Json para guardar información: ID del cliente, Token y Keys del cliente y del SERVER.
+        fs.existsSync('./BlackPanda.json') && BlackPanda.loadAuthInfo('./BlackPanda.json')
 
-        client.on('connecting', () => {
-        console.log(' intentando Conectando')
+//Conectando o reconectando
+        BlackPanda.on('connecting', () => {
+        console.log('Conectando')
         })
 
-        client.on('open', () => {
+//La conexión fue en éxito👌🏻
+        BlackPanda.on('open', () => {
         console.log('Conectado exitosamente :D')
         })
-        await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./BlackPanda.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-        }
+        await BlackPanda.connect({timeoutMs: 30*1000})
+        fs.writeFileSync('./BlackPands.json', JSON.stringify(BlackPanda.base64EncodedAuthInfo(), null, '\t'))
+        
 
+BlackPanda.on('chat-update', async (sam) => {
+try {	  
+if (!sam.hasNewMessage) return
+if (!sam.messages) return
+if (sam.key && sam.key.remoteJid == 'status@broadcast') return
+
+sam = sam.messages.all()[0]
+if (!sam.message) return
+global.blocked
+sam.message = (Object.keys(sam.message)[0] === 'ephemeralMessage') ? sam.message.ephemeralMessage.message : sam.message
+const from = sam.key.remoteJid
+const type = Object.keys(sam.message)[0]        
+const quoted = type == 'extendedTextMessage' && sam.message.extendedTextMessage.contextInfo != null ? sam.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+const typeQuoted = Object.keys(quoted)[0]
+const content = JSON.stringify(sam.message)
+const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+const body = sam.message.conversation || sam.message[type].caption || sam.message[type].text || ""
+chats = (type === 'conversation') ? sam.message.conversation : (type === 'extendedTextMessage') ? sam.message.extendedTextMessage.text : ''
+budy = (type === 'conversation' && sam.message.conversation.startsWith(prefix)) ? sam.message.conversation : (type == 'imageMessage') && sam.message.imageMessage.caption.startsWith(prefix) ? sam.message.imageMessage.caption : (type == 'videoMessage') && sam.message.videoMessage.caption.startsWith(prefix) ? sam.message.videoMessage.caption : (type == 'extendedTextMessage') && sam.message.extendedTextMessage.text.startsWith(prefix) ? sam.message.extendedTextMessage.text : ''
+
+if (prefix != "") {
+if (!body.startsWith(prefix)) {
+cmd = false
+comm = ""
+} else {
+cmd = true
+comm = body.slice(1).trim().split(" ").shift().toLowerCase()
+}
+} else {
+cmd = false
+comm = body.trim().split(" ").shift().toLowerCase()
+}
+        
+const command = comm
+
+const arg = chats.slice(command.length + 2, chats.length)
+const args = budy.trim().split(/ +/).slice(1)
+const isCmd = budy.startsWith(prefix)
+const q = args.join(' ')
+const soyYo = BlackPanda.user.jid
+const botNumber = BlackPanda.user.jid.split("@")[0]
+const ownerNumber = ['50762146191@s.whatsapp.net']
+const isGroup = from.endsWith('@g.us')
+const sender = sam.key.fromMe ? BlackPanda.user.jid : isGroup ? sam.participant : sam.key.remoteJid
+const senderNumber = sender.split("@")[0]
+const isMe = senderNumber == botNumber
+const conts = sam.key.fromMe ? BlackPanda.user.jid : BlackPanda.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+const pushname = sam.key.fromMe ? BlackPanda.user.name : conts.notify || conts.vname || conts.name || '-'
+
+switch (command) {
+
+case 'bot':
+client.sendMessage(from, 'Hola,felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted : sam})
+break
+                
+}
+
+} catch (e) {
+        
+console.log(e)}
+        
+})      
+}
 iniciar ()
 .catch (err => console.log("unexpected error: " + err))
